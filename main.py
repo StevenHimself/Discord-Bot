339f6bb03dcdb384e7b40b06b55f6410d9d65e9b
StevenHimself <zendrick7@gmail.com>
2023-06-10 14:11:47 -0400

    added /dog and /cat commands spits random images

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
 #Author: Steven Montecinos
 
-import discord
+import discord, aiohttp, json, os
 from discord import app_commands
 from discord.ext import commands
-import json
-import os
 
 if os.path.exists(os.getcwd() + "/config.json") :
     #pass //this is a dummy pass to generate a json file that will secure the bot's token
@@ -31,7 +29,7 @@ async def on_ready():
     except Exception as e:
         print(e)
 
-#commands
+#text based commands
 @bot.tree.command(name="rayjay")
 async def rayjay(interaction: discord.Interaction):
     await interaction.response.send_message(f"Hey there little {interaction.user.mention}! it's Ray Jay time! RAYJAY RAYJAY RAYJAY")
@@ -48,6 +46,32 @@ async def littlegirl(interaction: discord.Interaction):
 @app_commands.describe(thing_to_say= "user input")
 async def say(interaction: discord.Interaction, thing_to_say: str):
     await interaction.response.send_message(f"{interaction.user.name} said: {thing_to_say}")
+
+
+#image based commands
+@bot.tree.command(name="cat", description="Generates a random cat pic")
+@app_commands.checks.cooldown(1,5,key = lambda i : (i.user.id))
+async def cat_pic(interaction : discord.Interaction) : 
+    await interaction.response.defer
+    async with aiohttp.ClientSession() as session:
+        async with session.get('https://api.thecatapi.com/v1/images/search') as response:
+            raw = await response.text()
+            cat = json.loads(raw)[0]
+            embed = discord.Embed(title="Cat", color=discord.Colour.green())
+            embed.set_image(url= cat['url'])
+            await interaction.followup.send(embed=embed)
+
+@bot.tree.command(name="dog", description="Generates a random dog pic")
+@app_commands.checks.cooldown(1,5,key = lambda i : (i.user.id))
+async def dog_pic(interaction : discord.Interaction) : 
+    await interaction.response.send_message.defer
+    async with aiohttp.ClientSession() as session:
+        async with session.get('https://dog.ceo/api/breeds/image/random') as response:
+            raw = await response.text()
+            dog = json.loads(raw)[0]
+            embed = discord.Embed(title="Dog", color=discord.Colour.green())
+            embed.set_image(url= dog['url'])
+            await interaction.followup.send(embed=embed)
 
 bot.run(token)
 
