9a3413380925ac050b572a5f65314685a8ebae4b
StevenHimself <zendrick7@gmail.com>
2023-07-08 21:46:06 -0400

    Added emojis to the dog and cat commands

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
-#Author: Steven Montecinos
+# Author: Steven Montecinos
 
-import discord, aiohttp, json, os
+import discord, aiohttp, json, os, emoji
 from discord import app_commands
 from discord.ext import commands
 
-if os.path.exists(os.getcwd() + "/config.json") :
-    #pass //this is a dummy pass to generate a json file that will secure the bot's token
+# secure token
+if os.path.exists(os.getcwd() + "/config.json"):
+    # pass //this is a dummy pass to generate a json file that will secure the bot's token
     with open("./config.json") as f:
         configData = json.load(f)
 else:
-    configTemplate = {"Token" : "", "Prefix" : "!"}
+    configTemplate = {"Token": "", "Prefix": "!"}
 
     with open(os.getcwd() + "/config.json", "w+") as f:
         json.dump(configTemplate, f)
@@ -19,7 +20,8 @@ prefix = configData["Prefix"]
 
 bot = commands.Bot(command_prefix="!", intents=discord.Intents.all(), application_id=1116698756046389300)
 
-#startup
+
+# startup
 @bot.event
 async def on_ready():
     print("Bot online, Beep Boop.")
@@ -29,60 +31,72 @@ async def on_ready():
     except Exception as e:
         print(e)
 
-#text based commands
+
+# text based commands
 @bot.tree.command(name="rayjay", description="It's going to be RayJay time!")
 async def rayjay(interaction: discord.Interaction):
-    await interaction.response.send_message(f"Hey there little {interaction.user.mention}! it's Ray Jay time! RAYJAY RAYJAY RAYJAY")
+    await interaction.response.send_message(
+        f"Hey there little {interaction.user.mention}! it's Ray Jay time! RAYJAY RAYJAY RAYJAY")
+
 
 @bot.tree.command(name="battlecry", description="Join Dustin into battle!")
 async def battlecry(interaction: discord.Interaction):
-    await interaction.response.send_message(f"BROTHERS OF THE NORTHERN REGION JOIN ME INTO BATTLE TO BREAK FATHER CALEB'S LEGS!!!  ...both of them")
+    await interaction.response.send_message(
+        f"BROTHERS OF THE NORTHERN REGION JOIN ME INTO BATTLE TO BREAK FATHER CALEB'S LEGS!!!  ...both of them")
+
 
 @bot.tree.command(name="littlegirl", description="Steph's catchphrase")
 async def littlegirl(interaction: discord.Interaction):
     await interaction.response.send_message(f"{interaction.user.mention} OH MY GOODNESS!")
 
+
 @bot.tree.command(name="say", description="Repeats your input")
-@app_commands.describe(thing_to_say= "user input")
+@app_commands.describe(thing_to_say="user input")
 async def say(interaction: discord.Interaction, thing_to_say: str):
     await interaction.response.send_message(f"{interaction.user.name} said: {thing_to_say}")
 
+
 @bot.tree.command(name="pauliejoke", description="Repeats your joke with a Sopranos twist!")
-@app_commands.describe(paulie_joke= "user input")
+@app_commands.describe(paulie_joke="user input")
 async def say(interaction: discord.Interaction, paulie_joke: str):
     await interaction.response.send_message(f"Ay Ton' you hear what I said? I said {paulie_joke} HEH HEH")
 
 
-#image based commands
+# image/gif based commands
 @bot.tree.command(name="cat", description="Generates a random cat image/gif")
-@app_commands.checks.cooldown(1,5,key = lambda i : (i.user.id))
-async def catpic(interaction : discord.Interaction): 
+@app_commands.checks.cooldown(1, 5, key=lambda i: (i.user.id))
+async def catpic(interaction: discord.Interaction):
     await interaction.response.defer()
     async with aiohttp.ClientSession() as session:
         async with session.get('https://api.thecatapi.com/v1/images/search') as response:
             raw = await response.text()
             cat = json.loads(raw)[0]
-            embed = discord.Embed(title="Cat", color=discord.Colour.red())
-            embed.set_image(url= cat['url'])
+            embed = discord.Embed(title=emoji.emojize("meow! :grinning_cat:"), color=discord.Colour.purple())
+            embed.set_image(url=cat['url'])
             await interaction.followup.send(embed=embed)
 
+
 @bot.tree.command(name="dog", description="Generates a random dog image/gif")
-@app_commands.checks.cooldown(1,5,key = lambda i : (i.user.id))
-async def dogpic(interaction : discord.Interaction): 
+@app_commands.checks.cooldown(1, 5, key=lambda i: (i.user.id))
+async def dogpic(interaction: discord.Interaction):
     await interaction.response.defer()
     async with aiohttp.ClientSession() as session:
         async with session.get('https://dog.ceo/api/breeds/image/random') as response:
             raw = await response.text()
             dog = json.loads(raw)
-            embed = discord.Embed(title="Dog", color=discord.Colour.green())
-            embed.set_image(url= dog['message'])
+            embed = discord.Embed(title=emoji.emojize("woof! :dog_face:"), color=discord.Colour.green())
+            embed.set_image(url=dog['message'])
             await interaction.followup.send(embed=embed)
+
 
 @bot.tree.error
 async def on_app_command_error(interaction: discord.Interaction, error: app_commands.AppCommandError):
-    if isinstance(error,app_commands.CommandOnCooldown):
+    if isinstance(error, app_commands.CommandOnCooldown):
         await interaction.response.send_message(error, ephemeral=True)
-    else: raise error
+    else:
+        raise error
+
+
+# TODO:music based commands
 
 bot.run(token)
-
