df87c84c640f99a9085e9db04e14f9c011a2ce12
StevenHimself <zendrick7@gmail.com>
2023-08-08 13:04:03 -0400

    Converted to slash commmands and added more embeds

--- cogs/Music.py
+++ cogs/Music.py
@@ -1,9 +1,10 @@
 import discord
 import wavelink
+from discord import app_commands
 from discord.ext import commands
 
 
-class MusicCommands(commands.Cog):
+class Music(commands.Cog):
 
     def __init__(self, bot):
         self.bot = bot
@@ -13,147 +14,168 @@ class MusicCommands(commands.Cog):
         print("Music commands loaded ✔️")
 
     # command that plays from YouTube
-    @commands.command()
-    async def yplay(self, ctx: commands.Context, *, track: str):
-        vc = ctx.voice_client  # represents a discord connection
+    @app_commands.command(name='yplay', description='Plays a song from YouTube!')
+    async def yplay(self, interaction: discord.Interaction, *, track: str):
+        await interaction.response.defer()
+        vc = interaction.guild.voice_client  # represents a discord connection
         tracks = await wavelink.YouTubeTrack.search(track)
-
         if not vc:
             # custom_player = CustomPlayer()
-            vc: wavelink.Player = await ctx.author.voice.channel.connect(cls=wavelink.Player, self_deaf=True)
+            vc: wavelink.Player = await interaction.user.voice.channel.connect(cls=wavelink.Player, self_deaf=True)
 
         if not vc.is_playing() and vc.queue.is_empty:
             await vc.play(tracks[0])
 
             embed = discord.Embed(title=tracks[0].title, color=discord.Colour.red(), url=tracks[0].uri,
                                   description="Playing from YouTube! ▶️")
-            embed.set_footer(text=f"Request made by {ctx.author}", icon_url=ctx.author.display_avatar)
-
-            await ctx.send(embed=embed)
+            embed.set_footer(text=f"Request made by {interaction.user}", icon_url=interaction.user.display_avatar)
+            await interaction.followup.send(embed=embed)
 
         else:
             vc.queue.put(item=tracks[0])
 
             embed = discord.Embed(title=tracks[0].title, color=discord.Colour.red(), url=tracks[0].uri,
                                   description="Queued from YouTube ▶️")
-            embed.set_footer(text=f"Request made by {ctx.author}", icon_url=ctx.author.display_avatar)
+            embed.set_footer(text=f"Request made by {interaction.user}", icon_url=interaction.user.display_avatar)
 
-            await ctx.send(embed=embed)
+            await interaction.followup.send(embed=embed)
 
     # command that plays from SoundCloud
-    @commands.command()
-    async def splay(self, ctx: commands.Context, *, track: str):
-        vc = ctx.voice_client
+    @app_commands.command(name='splay', description='Plays a song from SoundCloud!')
+    async def splay(self, interaction: discord.Interaction, *, track: str):
+        await interaction.response.defer()
+        vc = interaction.guild.voice_client
         tracks = await wavelink.SoundCloudTrack.search(track)
 
         if not vc:
             # custom_player = CustomPlayer()
-            vc: wavelink.player = await ctx.author.voice.channel.connect(cls=wavelink.Player)
+            vc: wavelink.player = await interaction.user.voice.channel.connect(cls=wavelink.Player, self_deaf=True)
 
         if not vc.is_playing() and vc.queue.is_empty:
             await vc.play(tracks[0])
 
             embed = discord.Embed(title=tracks[0].title, color=discord.Colour.orange(), url=tracks[0].uri,
-                                      description="Playing from SoundCloud! ☁️")
-            embed.set_footer(text=f"Request made by {ctx.author}", icon_url=ctx.author.display_avatar)
+                                  description="Playing from SoundCloud! ☁️")
+            embed.set_footer(text=f"Request made by {interaction.user}", icon_url=interaction.user.display_avatar)
 
-            await ctx.send(embed=embed)
+            await interaction.followup.send(embed=embed)
 
         else:
             vc.queue.put(item=tracks[0])
 
             embed = discord.Embed(title=tracks[0].title, color=discord.Colour.orange(), url=tracks[0].uri,
-                                      description="Queued from SoundCloud! ☁️")
-            embed.set_footer(text=f"Request made by {ctx.author}", icon_url=ctx.author.display_avatar)
-
-            await ctx.send(embed=embed)
+                                  description="Queued from SoundCloud! ☁️")
+            embed.set_footer(text=f"Request made by {interaction.user}", icon_url=interaction.user.display_avatar)
+            await interaction.followup.send(embed=embed)
 
     # skip command
-    @commands.command()
-    async def skip(self, ctx: commands.Context):
-        vc = ctx.voice_client
+    @app_commands.command(name='skip', description='Skips current track.')
+    async def skip(self, interaction: discord.Interaction):
+        await interaction.response.defer()
+        vc = interaction.guild.voice_client
         if vc:
             if not vc.is_playing():
-                return await ctx.send("I am not playing anything to skip.")
+                embed = discord.Embed(title="I am not playing anything to skip. 🤨", color=discord.Colour.blurple())
+                interaction.followup.send(embed=embed)
             else:
-                embed = discord.Embed(title="Track was skipped! ⏩", color=discord.Colour.blurple())
-                embed.set_footer(text=f"Skipped by {ctx.author}", icon_url=ctx.author.display_avatar)
-                await ctx.send(embed=embed)
                 await vc.stop()
+
+                embed = discord.Embed(title="Track was skipped! ⏩", color=discord.Colour.blurple())
+                embed.set_footer(text=f"Skipped by {interaction.user}", icon_url=interaction.user.display_avatar)
+                await interaction.followup.send(embed=embed)
         else:
-            await ctx.send("I am not connected to a voice channel.")
+            embed = discord.Embed(title="I am not connected to a voice channel. 😔", color=discord.Colour.blurple())
+            await interaction.followup.send(embed=embed)
 
     # pause command
-    @commands.command()
-    async def pause(self, ctx: commands.Context):
-        vc = ctx.voice_client
+    @app_commands.command(name='pause', description='Pauses the current track.')
+    async def pause(self, interaction: discord.Interaction):
+        await interaction.response.defer()
+        vc = interaction.guild.voice_client
         if vc:
             if vc.is_paused():
-                return await ctx.send("I am already paused.")
+                embed = discord.Embed(title="I am already paused. 🤨", color=discord.Colour.blurple())
+                interaction.followup.send(embed=embed)
             else:
-                embed = discord.Embed(title="Track was paused! ⏸️", color=discord.Colour.blurple())
-                embed.set_footer(text=f"Paused by {ctx.author}", icon_url=ctx.author.display_avatar)
-                await ctx.send(embed=embed)
                 await vc.pause()
 
+                embed = discord.Embed(title="Track was paused! ⏸️", color=discord.Colour.blurple())
+                embed.set_footer(text=f"Paused by {interaction.user}", icon_url=interaction.user.display_avatar)
+                await interaction.followup.send(embed=embed)
         else:
-            await ctx.send("I am not connected to a voice channel.")
+            embed = discord.Embed(title="I am not connected to a voice channel. 😔", color=discord.Colour.blurple())
+            await interaction.followup.send(embed=embed)
 
-        # resume command
-    @commands.command()
-    async def resume(self, ctx: commands.Context):
-        vc = ctx.voice_client
+    # resume command
+    @app_commands.command(name='resume', description='Resume the current track.')
+    async def resume(self, interaction: discord.Interaction):
+        await interaction.response.defer()
+        vc = interaction.guild.voice_client
         if vc:
             if not vc.is_paused():
-                return await ctx.send("I'm already playing music.")
+                embed = discord.Embed(title="I am already playing music. 🤨", color=discord.Colour.blurple())
+                interaction.followup.send(embed=embed)
             else:
-                embed = discord.Embed(title="Track was resumed! 🎵", color=discord.Colour.blurple())
-                embed.set_footer(text=f"Resumed by {ctx.author}", icon_url=ctx.author.display_avatar)
-                await ctx.send(embed=embed)
                 await vc.resume()
 
+                embed = discord.Embed(title="Track was resumed! 🎵", color=discord.Colour.blurple())
+                embed.set_footer(text=f"Resumed by {interaction.user}", icon_url=interaction.user.display_avatar)
+                await interaction.followup.send(embed=embed)
         else:
-            await ctx.send("I am not connected to a voice channel.")
+            embed = discord.Embed(title="I am not connected to a voice channel. 😔", color=discord.Colour.blurple())
+            await interaction.followup.send(embed=embed)
 
     # displays current queue
-    @commands.command()
-    async def queue(self, ctx: commands.Context):
-        vc = ctx.voice_client
+    @app_commands.command(name='queue', description='Displays the current queue.')
+    async def queue(self, interaction: discord.Interaction):
+        await interaction.response.defer()
+        vc = interaction.guild.voice_client
         if vc:
-            embed = discord.Embed(title="Current Queue", color=discord.Colour.blurple(), description=f'{vc.queue}')
-            await ctx.send(embed=embed)
+            if not vc.queue.is_empty:
+                embed = discord.Embed(title="Current queue", color=discord.Colour.blurple(), description=f'{vc.queue}')
+                await interaction.followup.send(embed=embed)
 
-        else:
-            await ctx.send("I am not connected to a voice channel.")
+            elif vc.queue.is_empty:
+                embed = discord.Embed(title="Queue is empty... 😔", color=discord.Colour.blurple())
+                await interaction.followup.send(embed=embed)
+
+        elif not vc:
+            embed = discord.Embed(title="I am not connected to a voice channel. 😔", color=discord.Colour.blurple())
+            await interaction.followup.send(embed=embed)
 
     # connect to channel command
-    @commands.command()
-    async def connect(self, ctx: commands.Context, *, channel: discord.VoiceChannel | None = None):
-        vc = ctx.voice_client
+    @app_commands.command(name='connect', description='Connects bot to your current voice channel..')
+    async def connect(self, interaction: discord.Interaction, *, channel: discord.VoiceChannel | None = None):
+        await interaction.response.defer()
+        vc = interaction.guild.voice_client
         try:
-            channel = channel or ctx.author.voice.channel
+            channel = channel or interaction.user.voice.channel
         except AttributeError:
-            return await ctx.send("No voice channel to connect to. Please either provide one or join one.")
+            interaction.followup.send("No voice channel to connect to. Please either provide one or join one.")
 
         if not vc:
             await channel.connect(cls=wavelink.Player())
-            embed = discord.Embed(title=f"Connected to {ctx.author.voice.channel} ✅", color=discord.Colour.green())
-            await ctx.send(embed=embed)
+            embed = discord.Embed(title=f"Connected to {interaction.user.voice.channel} ✅",
+                                  color=discord.Colour.green())
+            await interaction.followup.send(embed=embed)
         elif vc:
-            await ctx.send("I am already connected to a channel.")
+            embed = discord.Embed(title="I am already connected to a voice channel. 🤨", color=discord.Colour.blurple())
+            await interaction.followup.send(embed=embed)
 
     # disconnect from channel command
-    @commands.command()
-    async def disconnect(self, ctx: commands.Context, *, channel: discord.VoiceChannel | None = None):
-        vc = ctx.voice_client
+    @app_commands.command(name='disconnect', description='Disconnects bot from voice channel.')
+    async def disconnect(self, interaction: discord.Interaction):
+        await interaction.response.defer()
+        vc = interaction.guild.voice_client
         if vc:
             await vc.disconnect()
-            embed = discord.Embed(title=f"Disconnected from {ctx.author.voice.channel} 👋",
+            embed = discord.Embed(title=f"Disconnected from {interaction.user.voice.channel}. 👋",
                                   color=discord.Colour.dark_red())
-            await ctx.send(embed=embed)
+            await interaction.followup.send(embed=embed)
         elif not vc:
-            await ctx.send("I am not connected to voice channel!")
+            embed = discord.Embed(title="I am not connected to a voice channel. 🤨", color=discord.Colour.blurple())
+            await interaction.followup.send(embed=embed)
 
 
 async def setup(bot):
-    await bot.add_cog(MusicCommands(bot))
+    await bot.add_cog(Music(bot))
