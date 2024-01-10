917c010e6d80f66929cb04c953517bcc5a404cef
StevenHimself <zendrick7@gmail.com>
2024-01-09 19:17:27 -0500

    Added help command skeleton

--- cogs/Text.py
+++ cogs/Text.py
@@ -16,6 +16,11 @@ class Text(commands.Cog):
     async def on_ready(self):
         print("Text commands loaded ✔️")
 
+    #TODO
+    # a command that lists all commands and their uses
+
+    # async def help(self, interaction: discord.Interaction):
+
     @app_commands.command(name="quote", description="Generates a random quote from different categories")
     @app_commands.checks.cooldown(1, 5, key=lambda i: i.user.id)
     @app_commands.choices(categories=[app_commands.Choice(name="anger", value="anger"),
