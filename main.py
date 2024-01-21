b7fe103b40189b2810429e496108e267075cb71a
StevenHimself <zendrick7@gmail.com>
2024-01-21 15:44:34 -0500

    Added conditional for auto shutdown on every branch that's not master

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # Author: Steven Montecinos
 import discord
+import subprocess
 import asyncio
-import logging
 import os
 from discord.ext import commands
 from dotenv import load_dotenv
@@ -11,12 +11,19 @@ token = os.getenv('DISCORD_TOKEN')
 
 # initiating bot
 bot = commands.Bot(command_prefix="!", intents=discord.Intents.all(), application_id=1116698756046389300)
-discord.utils.setup_logging(level=logging.INFO)
+
+
+async def get_current_branch():
+    """gets current working branch"""
+    try:
+        return subprocess.check_output(["git", "rev-parse", "--abbrev-ref", "HEAD"], universal_newlines=True)
+    except subprocess.CalledProcessError:
+        return None
 
 
 async def shutdown_after_delay():
     """shuts down bot after delay"""
-    await asyncio.sleep(7)
+    await asyncio.sleep(60)
     await bot.close()
 
 
@@ -34,7 +41,14 @@ async def main():
     async def on_ready():
         """starts up bot"""
         print("Stubee Bot online, Beep Boop. 🤖")
-        bot.loop.create_task(shutdown_after_delay())  # leave this on when pushing to main to prevent hanging workflow
+
+        current_branch = await get_current_branch()
+
+        if current_branch != "master":
+            print("This is a non-master branch, bot will shut down in 1 minute.")
+            bot.loop.create_task(shutdown_after_delay())
+        else:
+            print("You are on a master branch, bot will continue running until user intervention.")
 
     await bot.start(token)
 
